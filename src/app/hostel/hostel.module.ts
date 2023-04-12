0ca60c3213b755463bb23d2675b7536b58b2f5c2
Safwankaprakkadan <safwankc1999@gmail.com>
2023-04-12 12:49:06 +0530

    commit

--- src/app/hostel/hostel.module.ts
+++ src/app/hostel/hostel.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RoomVacateComponent } from './room-vacate/room-vacate.component';
+import { AdmissionEntryComponent } from './admission-entry/admission-entry.component';
+import { AddRoomComponent } from './add-room/add-room.component';
+import { AssetsComponent } from './assets/assets.component';
 
 
 
 @NgModule({
   declarations: [
-    RoomVacateComponent
+    RoomVacateComponent,
+    AdmissionEntryComponent,
+    AddRoomComponent,
+    AssetsComponent
   ],
   imports: [
     CommonModule
